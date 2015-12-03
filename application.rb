94c76f3df0d864330ccbf538ac2af6e9b2a7fb0f
Jordan Burke <me@jordankburke.com>
2015-12-03 18:16:18 -0500

    Working on headers

--- application.rb
+++ application.rb
@@ -6,11 +6,17 @@ require 'pony'
 require 'erb'
 
 class SmallPotato < Sinatra::Base
-  set :protection, false
+  before do
+    content_type :json
+    headers 'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => ['POST']
+  end
 
   Dir["./models/*.rb"].each { |file| require file }
 
   post '/send_email' do
+    headers 'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => ['POST']
     res = Pony.mail(
       :from => params[:name] + "<" + params[:email] + ">",
       :to => 'info@justsmallpotatoes.com',
@@ -34,6 +40,8 @@ class SmallPotato < Sinatra::Base
   end
 
   post '/send_order' do
+    headers 'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => ['POST']
     charge = Charge.new(ENV['STRIPE_KEY'])
     charge.newOrder(params[:token], params[:amount])
     template_path = "./email.html.erb"
@@ -55,7 +63,7 @@ class SmallPotato < Sinatra::Base
       })
     content_type :json
     if res
-      { :message => 'success', :charge => charge }.to_json
+      { :message => 'success' }.to_json
     else
       { :message => 'failure_email' }.to_json
     end
@@ -65,4 +73,12 @@ class SmallPotato < Sinatra::Base
     File.read('_site/404.html')
   end
 
+  get '/*' do
+    file_name = "_site#{request.path_info}/index.html".gsub(%r{\/+},'/')
+    if File.exists?(file_name)
+      File.read(file_name)
+    else
+      raise Sinatra::NotFound
+    end
+  end
 end
